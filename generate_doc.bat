8f0980fb1b4e362abadf98985dad84717c059789
wronex <wronex@gmail.com>
2012-03-10 22:40:37 +0100

    I still dont know how git works...

--- generate_doc.bat
+++ generate_doc.bat
@@ -2,8 +2,5 @@
 set jsdoc=./
 set output=./doc/
 set input=./app
-::node "%jsdoc%/app/run.node.js" --help
-::node "%jsdoc%/app/run.node.js" -c=conf/sample.conf
 node  "%jsdoc%/app/run.node.js" -a -d=%output% -t="%jsdoc%/templates/WouterBos" -r %input%
-::java -jar "%jsdoc%/jsrun.jar" "%jsdoc%/app/run.js" -a -d=%output% -s -r -t="%jsdoc%/templates/jsdoc" %input%
 pause
